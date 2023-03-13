c78e4ca054edd52043f948db6adb4e712b50fec2
Guillem <you@example.com>
2023-03-13 10:32:41 +0100

    Describe why there's a clean.mjs file

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,5 +1,8 @@
 import fs from 'fs'
 
+// This ensures a cross-platform way of removing old / dirty code,
+// so that we can use it in our build npm scripts
+
 // We don't need to handle errors here, because we don't care whether the directories exist
 try {
     fs.rmSync('./dist', { recursive: true })
