50d805fc3d5937a62158a1a3bb990c44d0e5f6f0
Guillem <you@example.com>
2023-03-17 18:50:56 +0100

    Add tests for POST reviews

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -12,7 +12,7 @@ const testURL = `http://localhost:${config.port}`
 
 describe('integration tests', () => {
     test('returns 404 for non-existent business', async () => {
-        const logger = (lvl: core.LogLevels, msg: string) => {}
+        const logger = (lvl: core.LogLevels, msg: string) => { }
         const store = createInMemDb()
         const startedServer = startServer(logger, store)
 
@@ -23,7 +23,7 @@ describe('integration tests', () => {
     })
 
     test('returns 200 for existing business with its corresponding details', async () => {
-        const logger = (lvl: core.LogLevels, msg: string) => {}
+        const logger = (lvl: core.LogLevels, msg: string) => { }
         const inmemStore = createInMemDb()
         const onlineBusinessRes = await inmemStore.business.createOnlineBusiness({
             name: 'test',
@@ -42,11 +42,79 @@ describe('integration tests', () => {
 
         const startedServer = startServer(logger, inmemStore)
 
-        let response = await fetch(`${testURL}/business/${onlineBusinessRes.value.id}`)
+        { // online business
+            const response = await fetch(`${testURL}/business/${onlineBusinessRes.value.id}`)
+            expect(response.status).toBe(200)
+
+            const json = await response.json()
+            expect(json).toStrictEqual({
+                id: '1',
+                name: 'test',
+                website: 'test.com',
+                email: 'test@test.com',
+                total_reviews: 0
+            })
+        }
+
+        { // physical business
+            const response = await fetch(`${testURL}/business/${physicalBusiness.value.id}`)
+            expect(response.status).toBe(200)
+
+            const json = await response.json()
+            expect(json).toStrictEqual({
+                id: '2',
+                name: 'test',
+                address: '123 test st',
+                phone: '1234567890',
+                email: 'test@test.com',
+                total_reviews: 0
+            })
+        }
+
+        await startedServer.stop()
+    })
+
+    test('create and retrieve reviews for a business correctly work', async () => {
+        const logger = (lvl: core.LogLevels, msg: string) => { }
+        const inmemStore = createInMemDb()
+        const onlineBusinessRes = await inmemStore.business.createOnlineBusiness({
+            name: 'test',
+            email: 'test@test.com',
+            website: 'test.com'
+        })
+        if (onlineBusinessRes.type !== 'success') throw new Error('Failed to create business')
+
+        const startedServer = startServer(logger, inmemStore)
+
+        // add reviews
+        for (let rating = 1; rating < 5; rating++) {
+            const response = await fetch(`${testURL}/business/${onlineBusinessRes.value.id}/reviews`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    text: 'super amazing business test test lets get to more than 20 chars',
+                    rating,
+                    username: 'test user 1',
+                })
+            })
+
+            expect(response.status).toBe(200)
+        }
+
+        // get reviews
+        const response = await fetch(`${testURL}/business/${onlineBusinessRes.value.id}`)
         expect(response.status).toBe(200)
 
-        response = await fetch(`${testURL}/business/${physicalBusiness.value.id}`)
-        expect(response.status).toBe(200)
+        const json = await response.json()
+        expect(json).toStrictEqual({    
+            id: '1',
+            name: 'test',
+            website: 'test.com',
+            email: 'test@test.com',
+            total_reviews: 4,
+        })
 
         await startedServer.stop()
     })
