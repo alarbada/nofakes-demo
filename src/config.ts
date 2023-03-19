22ec11bee09a0c989cb5535fa76c6a8c00dcd49a
Guillem <you@example.com>
2023-03-19 12:54:39 +0100

    Load env vars to configuration

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,22 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+function getEnv(name: string): string {
+    const value = process.env[name]
+    if (value === undefined) {
+        console.error(`Fatal error: Env var ${name} not found`)
+        process.exit(1)
+    }
+
+    return value
+}
+
 export default {
-    port: process.env["PORT"] ?? "3000"
+    port: getEnv("SERVER_PORT"),
+    mongo: {
+        dbName: getEnv("MONGO_DATABASE")
+    }
 }
 
 
